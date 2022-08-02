a25490f0528a220abb367a7ba975abcb533a440d
Artur Sveshnikov <archey.net@live.ru>
2022-08-02 17:34:39 +0300

    0208

---
+++ Shultc/Data/Form/ReceptionForm.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shultc.Data.Form
+{
+	public class ReceptionForm
+	{
+		[Required(ErrorMessage = "Напишите, пожалуйста, своё имя")]
+		[StringLength(10, ErrorMessage = "Имя слишком длинное")]
+		public string? Name { get; set; }
+		[Required(ErrorMessage = "Введите свой Email адрес")]
+		public string? Email { get; set; }
+		[Required(ErrorMessage = "Выберите тему обращения")]
+		public string? Subject { get; set; }
+		
+		public bool IsAgreeWithContract { get; set; }
+		[Required(ErrorMessage = "Сообщение пусто")]
+		public string? Message { get; set; }
+		public DateTime SendMessageDate { get; set; }
+
+	}
+}
